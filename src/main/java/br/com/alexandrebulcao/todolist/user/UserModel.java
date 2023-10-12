239c691d047abc56fbd261889b4e2359ca829115
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-10-12 17:21:42 +0100

    Adicionando DB ao Projeto
    Criando DB H2 Database Engine, adicionando o Spring Data JPA e as dependências project lombok, conectando ao jdbc:h2:mem:todolist

--- src/main/java/br/com/alexandrebulcao/todolist/user/UserModel.java
+++ src/main/java/br/com/alexandrebulcao/todolist/user/UserModel.java
@@ -1,7 +1,12 @@
 package br.com.alexandrebulcao.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   public String username;
   public String name;
   public String password;
+
+
 }
