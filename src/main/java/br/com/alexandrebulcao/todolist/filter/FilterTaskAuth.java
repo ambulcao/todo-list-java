7269cbeab625969b2f2e13935799b12d0ab2378d
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-10-13 15:22:23 +0100

    Filter Auth
    Criando um filter autentication para login de usuario

--- src/main/java/br/com/alexandrebulcao/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/alexandrebulcao/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,48 @@
 package br.com.alexandrebulcao.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    
-        System.out.println("Filtro de autenticação");
-        chain.doFilter(request, response);
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+        
+        //Pegar a autenticação (usuario e senha)
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
+        
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println(username);
+        System.out.println(password);
+
+
+        //Validar usuário
+
+        //Validar senha
+
+        // Restante do código
+
+        filterChain.doFilter(request, response);
+
   }
+
 
 }
