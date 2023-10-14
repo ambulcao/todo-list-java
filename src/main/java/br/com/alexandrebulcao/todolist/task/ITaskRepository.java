a08dc4be6364f3859b4223935dfd58a6950572a9
Alexandre Marques Bulcao <ambulcao@gmail.com>
2023-10-14 17:38:25 +0100

    Validando user/task
    Validando  user/task com o id da tarefa

--- src/main/java/br/com/alexandrebulcao/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/alexandrebulcao/todolist/task/ITaskRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
   List<TaskModel>findByIdUser(UUID idUser);
+  
 }
