387c49a690de9fdb2ee52770f07f3aded44d8b55
alexandrebulcao-aluclass <alexandre.bulcao@aluclass.pt>
2023-10-14 14:34:18 +0100

    Lista Tarefas
    Criando listas de tarefas com novo user

--- src/main/java/br/com/alexandrebulcao/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/alexandrebulcao/todolist/task/ITaskRepository.java
@@ -1,10 +1,11 @@
 package br.com.alexandrebulcao.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.scheduling.config.Task;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-  
+  List<TaskModel>findByIdUser(UUID idUser);
 }
